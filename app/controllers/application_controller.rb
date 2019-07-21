59c1cd478f37a71765b1fec6df69a6afef5ea20f
Siva <carah.tyche@gmail.com>
2019-07-21 16:14:43 -0500

    Added user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  def current_composer
+    auth_headers = request.headers['Authorization']
+    if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
+      token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
+      begin
+        decoded_token = JWT.decode(
+          token,
+          Rails.application.credentials.fetch(:secret_key_base),
+          true,
+          { algorithm: 'HS256' }
+        )
+        Composer.find_by(id: decoded_token[0]["composer_id"])
+      rescue JWT::ExpiredSignature
+        nil
+      end
+    end
+  end
+
+  helper_method :current_composer
+
+  def authenticate_composer
+    unless current_composer
+      render json: {}, status: :unauthorized
+    end
+  end
 end
+
