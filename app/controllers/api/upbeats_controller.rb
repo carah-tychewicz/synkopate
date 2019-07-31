8f799111013bd64de0dbf2150fea587b884ee063
Siva <carah.tyche@gmail.com>
2019-07-31 18:58:58 -0500

    adding routes

--- app/controllers/api/upbeats_controller.rb
+++ app/controllers/api/upbeats_controller.rb
@@ -1,15 +1,24 @@
 class Api::UpbeatsController < ApplicationController
+  def index
+    @upbeats = Upbeat.all
+    render "index.json.jb"
+  end
+
   def create
     @create_upbeat = Upbeat.new(
       title: params["title"],
       album: params["album"],
       duration: params["duration"].to_i,
       genre: params["genre"])
-      
-
     if @create_upbeat.save
       render "create.json.jb"
     else render json: { errors: @create_upbeat.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  def show
+    upbeat_id = params["id"]
+    @upbeat = Upbeat.find_by(id: upbeat_id)
+    render "show.json.jb"
   end
 end
