c404d01c2c2f4e926dd1a6cc1e9bfd4d933b2d10
Siva <carah.tyche@gmail.com>
2019-07-31 20:08:11 -0500

    added show route for upbeats

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/upbeats" => "upbeats#index"
+    get "/upbeats/:id" => "upbeats#show"
     post "/composers" => "composers#create"
     post "/sessions" => "sessions#create"
     post "/upbeats" => "upbeats#create"
