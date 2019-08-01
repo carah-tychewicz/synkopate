c53d4c95836d59170a4d6bff0e5401ad3f1f4aa5
Siva <carah.tyche@gmail.com>
2019-08-01 13:15:15 -0500

    working on Composer routes Just finished Upbeat.

--- app/controllers/api/composers_controller.rb
+++ app/controllers/api/composers_controller.rb
@@ -1,5 +1,10 @@
 class Api::ComposersController < ApplicationController
 
+  def index
+    @composers = Composer.all
+    render "index.json.jb"
+  end
+
   def create
     composer = Composer.new(
     username: params[:username],
@@ -7,13 +12,13 @@ class Api::ComposersController < ApplicationController
     password: params[:password],
     password_confirmation: params[:password_confirmation]
   )
-
     if composer.save
       render json: {message: 'Composer created successfully'}, status: :created
     else
       render json: {errors: composer.errors.full_messages}, status: :bad_request
     end
-
   end
+
+
 
 end
