752abca66f80051b61899ca44f23ddc129cb74cb
Siva <carah.tyche@gmail.com>
2019-07-31 19:54:52 -0500

    added route for index and json

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
+    get "/upbeats" => "upbeats#index"
     post "/composers" => "composers#create"
     post "/sessions" => "sessions#create"
     post "/upbeats" => "upbeats#create"
